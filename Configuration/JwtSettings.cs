4a6fbde938044a0929da155d1db09181e7fd938c
PAO <paufer2337@gmail.com>
2026-07-02 10:56:54 +0200

    Add JWT settings model

---
+++ Configuration/JwtSettings.cs
@@ -0,0 +1,15 @@
+
+
+
+
+namespace JwtAuthLab.Configuration;
+
+
+public class JwtSettings
+{
+    public string Secret { get; init; } = string.Empty;
+
+    public string Issuer { get; init; } = string.Empty;
+
+    public string Audience { get; init; } = string.Empty;
+}
